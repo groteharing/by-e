acd91ccfa2d306890c5491421875b75e771759a9
“groteharing” <hello@studiomoondust.com>
2022-03-25 15:44:03 +0100

    Prettier installed

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,16 +4,16 @@ module.exports = {
   theme: {
     extend: {},
     colors: {
-      'bye-white': '#ffffff',
-      'bye-black': '#000000',
-      'bye-earth': '#0F0A07',
-      'bye-grey': '#eeeeee',
-      'bye-taupe': {
-        50: '#E8E3DC',
-        100: '#A89D87',
-        200: '#665F52',
+      "bye-white": "#ffffff",
+      "bye-black": "#000000",
+      "bye-earth": "#0F0A07",
+      "bye-grey": "#eeeeee",
+      "bye-taupe": {
+        50: "#E8E3DC",
+        100: "#A89D87",
+        200: "#665F52",
       },
+    },
+    plugins: [],
   },
-  plugins: [],
-},
 };
