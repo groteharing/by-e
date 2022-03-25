1375fd87839f72b56d149beda179ae73e54b320a
“groteharing” <hello@studiomoondust.com>
2022-03-25 15:18:16 +0100

    Tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,17 @@ module.exports = {
   content: ["./public/**/*.html", "./src/**/*.{astro,js,jsx,ts,tsx,vue}"],
   theme: {
     extend: {},
+    colors: {
+      'bye-white': '#ffffff',
+      'bye-black': '#000000',
+      'bye-earth': '#0F0A07',
+      'bye-grey': '#eeeeee',
+      'bye-taupe': {
+        50: '#E8E3DC',
+        100: '#A89D87',
+        200: '#665F52',
+      },
   },
   plugins: [],
+},
 };
